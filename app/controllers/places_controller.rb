442b6de2aaffbeff5659d610be26ac619e1cb4c4
Andre Jensen <andrejensen302@gmail.com>
2017-02-20 01:18:07 +0000

    display locations on database on index page (FHP L6)

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,7 @@
 class PlacesController < ApplicationController
     
     def index
+        @places = Place.all #all the places in our database are stufed into a variable called @places.
     end
     
 end
