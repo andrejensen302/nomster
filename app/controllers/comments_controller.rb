bc5906ad83a0556ad0ea23388318ae37a112ad0e
Andre Jensen <andrejensen302@gmail.com>
2017-03-19 22:36:09 +0000

    FHP L33 create comments submit button and redirect user to place#show page)

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+    before_action :authenticate_user! #authenticate user before any comments are logged in
+    
+    def create
+        @place = Place.find(params[:place_id])
+        @place.comments.create(comment_params.merge(user: current_user)) #create comment that's connected to the place that's loaded
+        #merge method - combines two dictionaries (dictionary is a data structure known as a hash table)
+        redirect_to place_path(@place) #sends user back to place page after user creates the item
+    end
+    
+    private
+    
+    def comment_params #pull data out of the comments
+        params.require(:comment).permit(:message, :rating)
+    end
+end
