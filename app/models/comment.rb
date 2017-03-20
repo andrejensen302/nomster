3796f645f7de56a93e399b77a2604b41af4dcdc9
Andre Jensen <andrejensen302@gmail.com>
2017-03-20 00:31:43 +0000

    FHP L35 changed rating from text to drop down

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,17 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    
+    RATINGS = {
+     'one star': '1_star',
+     'two stars': '2_stars',
+     'three stars': '3_stars',
+     'four stars': '4_stars',
+     'five stars': '5_stars'
+    }  #for a rating system in the comments section
+    
+    def humanized_rating
+        RATINGS.invert[self.rating]
+    end
+    #allow us to call a humanized_rating from every single comment we look up.
 end
