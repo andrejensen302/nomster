3dc44917042d928a44cd3490e9a8238e2dc2749f
Andre Jensen <andrejensen302@gmail.com>
2017-03-25 22:57:21 +0000

    FHP L42 adjusting the automated mailer

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,10 @@
 class NotificationMailer < ApplicationMailer
      default from: 'no-reply@yelpclone.com'
    
-   def comment_added
-       mail(to: "andrejensen302@gmail.com", subject: "A comment has been added to your place")
+   def comment_added(comment) #will be able to access specific details about the comment that exists in our database
+       @place = comment.place #comment on a specific place
+       @place_owner = @place.user #the owner of the comment
+       mail(to: @place_owner.email, #mail to the comment owner's e-mail address
+            subject: "A comment has been added to #{@place.name}") #the comment will show the name of the specific place. Interpolation
    end
 end
