0e85315b2137776ea01aaf86faea83cfab39e4a8
Andre Jensen <andrejensen302@gmail.com>
2017-04-09 04:54:26 +0000

    e-mail updates

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -4,6 +4,7 @@ class NotificationMailer < ApplicationMailer
    def comment_added(comment) #will be able to access specific details about the comment that exists in our database
        @place = comment.place #comment on a specific place
        @place_owner = @place.user #the owner of the comment
+       
        mail(to: @place_owner.email, #mail to the comment owner's e-mail address
             subject: "A comment has been added to #{@place.name}") #the comment will show the name of the specific place. Interpolation
    end
