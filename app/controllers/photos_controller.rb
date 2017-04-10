a319afba5b6439a96f64dbcad15c3f245ea93a42
Andre Jensen <andrejensen302@gmail.com>
2017-04-10 00:03:01 +0000

    set up database to incporate photo uploads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,8 @@
 class PhotosController < ApplicationController
+    before_action :authenticate_user! #authenticate the user before any photos are added
+    
+    def create
+    
+    end
+    
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :places
-  has_many :comments
+  has_many :places #a user can create many places
+  has_many :comments #a user can create many comments
+  has_many :photos #a user can upload many photos
 end
