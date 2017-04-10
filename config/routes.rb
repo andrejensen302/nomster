847313f64addb79d9a438ca7ca8a1500014baeb2
Andre Jensen <andrejensen302@gmail.com>
2017-04-10 03:39:24 +0000

    Connected routes file for photo

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
     root 'places#index'
     resources :places do
-      resources :comments, only: :create
+      resources :comments, only: :create #allows posting to the comments controller and allows us to pass through a place_id, which we will use on our comments form.
+      resources :photos, only: :create #allows posting to the photos controller and allows us to pass through a place id.
     end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: :show
