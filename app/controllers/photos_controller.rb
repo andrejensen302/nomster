78cea12dcdc7ba9d932542cd7144ab47a97ccb5e
Andre Jensen <andrejensen302@gmail.com>
2017-04-10 01:07:16 +0000

    changes to photo controller. Def create

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,10 @@
 class PhotosController < ApplicationController
     before_action :authenticate_user! #authenticate the user before any photos are added
     
-    def create
-    
+    def create #save new photos into database
+        @place = Place.find(params[:id]) #load the place that the photo belongs too
+        @place.photos.create(photos_params.merge(user: current_user))
+        redirect_to place_path(@place) #redirect user to place once image is done uploading
     end
     
 end
