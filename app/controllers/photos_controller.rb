e451391cd615ba32085c509359d6d5d1f2e2eeae
Andre Jensen <andrejensen302@gmail.com>
2017-04-11 04:22:43 +0000

    Added photo modal button that takes user photo uploads

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -7,7 +7,7 @@ class PhotosController < ApplicationController
         redirect_to place_path(@place) #redirect user to place once image is done uploading
     end
     
-    def photo_params #pulls the values
+    def photo_params #pulls the picture values out of the form
         params.require(:photo).permit(:caption, :picture)
     end
     
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170410023546) do
+ActiveRecord::Schema.define(version: 20170411034319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170410023546) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "picture"
+    t.string   "caption"
   end
 
   create_table "places", force: :cascade do |t|
